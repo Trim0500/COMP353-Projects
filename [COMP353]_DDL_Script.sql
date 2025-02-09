b8f6db6497109557e3bf207635a3ad962d0f1e6a
icarus-2 <icarus-2@github.com>
2025-02-09 17:54:58 -0500

    DDL naming convention updates

--- [COMP353]_DDL_Script.sql
+++ [COMP353]_DDL_Script.sql
@@ -1,94 +1,94 @@
-create database if not exists WarmUpProject;
+CREATE DATABASE IF NOT EXISTS WarmUpProject;
 
-Create Table Personnel(
-	personnel_id int primary key auto_increment,
-    first_name varchar(50),
-    last_name varchar(50),
+CREATE TABLE Personnel(
+	personnel_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    date_of_birth DATE,
+    social_sec_number INT NOT NULL UNIQUE,
+    med_card_number INT NOT NULL UNIQUE,
+    phone_number CHAR(10),
+    address VARCHAR(50),
+    city VARCHAR(50),
+    province VARCHAR(2),
+    postal_code VARCHAR(6),
+    email_address VARCHAR(50),
+    role VARCHAR(50),
+    mandate VARCHAR(50)
+);
+
+CREATE TABLE Location(
+	location_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    city VARCHAR(50),
+    postal_code VARCHAR(6),
+    type VARCHAR(50),
+    address VARCHAR(100),
+    province VARCHAR(2),
+    capacity INT,
+    website VARCHAR(50)
+);
+
+CREATE TABLE LocationPhone(
+	location_id_fk INT,
+    phone_number CHAR(10),
+    PRIMARY KEY(location_id_fk, phone_number),
+    FOREIGN KEY(location_id_fk) REFERENCES Location(location_id)
+);
+
+CREATE TABLE PersonnelLocationDate(
+	personnel_id_fk INT,
+    location_id_fk INT,
+    start_date DATE,
+    end_date DATE,
+    PRIMARY KEY (personnel_id_fk, location_id_fk, start_date),
+    FOREIGN KEY(personnel_id_fk) REFERENCES Personnel(personnel_id),
+    FOREIGN KEY(location_id_fk) REFERENCES Location(location_id)
+);
+
+CREATE TABLE FamilyMember(
+	family_member_id int PRIMARY KEY AUTO_INCREMENT,
+	first_name VARCHAR(50),
+    last_name VARCHAR(50),
     date_of_birth date,
-    social_sec_number int not null unique,
-    med_card_number int not null unique,
+    social_sec_number INT NOT NULL UNIQUE,
+    med_card_number INT NOT NULL UNIQUE,
     phone_number char(10),
-    address varchar(50),
-    city varchar(50),
-    province varchar(2),
-    postal_code varchar(6),
-    email_address varchar(50),
-    role varchar(50),
-    mandate varchar(50)
+    address VARCHAR(100),
+    city VARCHAR(50),
+    province VARCHAR(2),
+    postal_code VARCHAR(6),
+    email_address VARCHAR(50),
+    location_id_fK INT,
+    FOREIGN KEY(location_id_fK) REFERENCES Location(location_id)
 );
 
-Create Table Location(
-	location_id int primary key auto_increment,
-    name varchar(50),
-    city varchar(50),
-    postal_code varchar(6),
-    type varchar(50),
-    address varchar(100),
-    province varchar(2),
-    capacity int,
-    website varchar(50)
+CREATE TABLE ClubMember(
+	member_id int PRIMARY KEY AUTO_INCREMENT,
+    family_member_id_fk INT,
+    relationship_type VARCHAR(20),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    date_of_birth DATE,
+    height FLOAT(5,2),
+    weight FLOAT(5,2),
+    social_sec_number INT NOT NULL UNIQUE,
+    med_card_number INT NOT NULL UNIQUE,
+    phone_number CHAR(10),
+    address VARCHAR(100),
+    city VARCHAR(50),
+    province VARCHAR(2),
+    postal_code VARCHAR(6),
+    FOREIGN KEY(family_member_id_fk) REFERENCES FamilyMember(family_member_id)
 );
 
-Create Table LocationPhone(
-	location_id_fk int,
-    phone_number char(10),
-    primary key(location_id_fk, phone_number),
-    foreign key(location_id_fk) references Location(location_id)
-);
-
-Create Table PersonnelLocationDate(
-	personnel_id_fk int,
-    location_id_fk int,
-    start_date date,
-    end_date date,
-    primary key (personnel_id_fk, location_id_fk, start_date),
-    foreign key(personnel_id_fk) references Personnel(personnel_id),
-    foreign key(location_id_fk) references Location(location_id)
-);
-
-Create Table FamilyMember(
-	family_member_id int primary key auto_increment,
-	first_name varchar(50),
-    last_name varchar(50),
-    date_of_birth date,
-    social_sec_number int not null unique,
-    med_card_number int not null unique,
-    phone_number char(10),
-    address varchar(100),
-    city varchar(50),
-    province varchar(2),
-    postal_code varchar(6),
-    email_address varchar(50),
-    location_id_fK int,
-    foreign key(location_id_fK) references Location(location_id)
-);
-
-Create Table ClubMember(
-	member_id int primary key auto_increment,
-    family_member_id_fk int,
-    relationship_type varchar(20),
-    first_name varchar(50),
-    last_name varchar(50),
-    date_of_birth date,
-    height float(5,2),
-    weight float(5,2),
-    social_sec_number int not null unique,
-    med_card_number int not null unique,
-    phone_number char(10),
-    address varchar(100),
-    city varchar(50),
-    province varchar(2),
-    postal_code varchar(6),
-    foreign key(family_member_id_fk) references FamilyMember(family_member_id)
-);
-
-Create Table Payment(
-	payment_id int primary key auto_increment,
-    member_id_fk int,
-    date date,
-    amount float(5,2),
-    method varchar(10),
-    effective_date date,
-    installment int,
-    foreign key(member_id_fk) references ClubMember(member_id)
+CREATE TABLE Payment(
+	payment_id int PRIMARY KEY AUTO_INCREMENT,
+    member_id_fk INT,
+    date DATE,
+    amount FLOAT(5,2),
+    method VARCHAR(10),
+    effective_date DATE,
+    installment INT,
+    FOREIGN KEY(member_id_fk) REFERENCES ClubMember(member_id)
 );
