24e1699d4adbb9866967ef4cf6c811ca292b2aa1
Tristan Lafleur <70395022+Trim0500@users.noreply.github.com>
2025-02-09 16:09:43 -0500

    Task[DML] Query 8

--- [COMP353]_DML_Queries_Script.sql
+++ [COMP353]_DML_Queries_Script.sql
@@ -4,12 +4,30 @@
 # Information includes date of payment, amount of payment, and year of payment. The results should be displayed sorted in ascending order by date.
 # ----------------------------------------
 
-Set @PaymentMemberID = 1;
+set @PaymentMemberID = 1;
 
-Select 
+select 
 	date as 'PaymentDate',
     amount as 'PaymentAmount',
     format(date, 'yyyy') as 'PaymentYear'
-From Payment
-Where member_id_fk = @PaymentMemberID
-Order By date;
+from Payment
+where member_id_fk = @PaymentMemberID
+order by date;
+
+# Query 8
+# ----------------------------------------
+# Get the sum of membership fees paid and the sum of donations that are collected by the club in the year 2024.
+# ----------------------------------------
+
+select 
+	case
+		when sum(amount) > 100.00 then 100.00
+        else sum(amount)
+	end as 'MembershipFeesAmount',
+    case
+		when sum(amount) > 100.00 then sum(amount) - 100.00
+        else 0.00
+	end as 'DonationsAmount'
+from Payment
+where format(effective_date, 'yyyy') = '2024'
+group by member_id_fk;
