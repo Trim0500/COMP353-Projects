9c94b04475b15d4848b7706e8f5e9e3c56f910d1
Tristan Lafleur <70395022+Trim0500@users.noreply.github.com>
2025-03-05 14:01:18 -0500

    Task[COMP353-Main-DML]: Create query 10

--- MainProject/[COMP353]_DML_Queries_Script.sql
+++ MainProject/[COMP353]_DML_Queries_Script.sql
@@ -2,45 +2,69 @@
 
 ALTER TABLE FamilyMember AUTO_INCREMENT = 1;
 
-Insert Into FamilyMember (first_name,last_name,dob,social_sec_num,med_card_num,city,province,phone_number,postal_code)
-Values ('Albein "Alm"','Rudolph II','1992-03-14','123456789','RUDA00020003','Ram','ZO','4383323524','A1M0RG');
+INSERT INTO FamilyMember (first_name,last_name,dob,social_sec_num,med_card_num,city,province,phone_number,postal_code)
+VALUES ('Albein "Alm"','Rudolph II','1992-03-14','123456789','RUDA00020003','Ram','ZO','4383323524','A1M0RG');
 
-Select *
-From FamilyMember;
+SELECT *
+FROM FamilyMember;
 
 Update FamilyMember
-Set first_name = 'Pin', last_name = 'Gas'
-Where id = 1;
+SET first_name = 'Pin', last_name = 'Gas'
+WHERE id = 1;
 
-Delete From FamilyMember
-Where id = 10;
+Delete FROM FamilyMember
+WHERE id = 10;
 
-Insert Into SecondaryFamilyMember
-Values (1,'Anthiese "Celica"','Lima V','4383327245');
+INSERT INTO SecondaryFamilyMember
+VALUES (1,'Anthiese "Celica"','Lima V','4383327245');
 
-Select *
-From SecondaryFamilyMember;
+SELECT *
+FROM SecondaryFamilyMember;
 
 Update SecondaryFamilyMember
-Set first_name = 'Dick', last_name = 'Tree'
-Where primary_family_member_id_fk = 1;
+SET first_name = 'Dick', last_name = 'Tree'
+WHERE primary_family_member_id_fk = 1;
 
-Delete From SecondaryFamilyMember
-Where primary_family_member_id_fk = 10;
+Delete FROM SecondaryFamilyMember
+WHERE primary_family_member_id_fk = 10;
 
 -- Query 4: CRUD for ClubMember
 
 ALTER TABLE ClubMember AUTO_INCREMENT = 1;
 
-Insert Into ClubMember (first_name,last_name,dob,height,weight,social_sec_num,med_card_num,phone_number,city,province,postal_code,progress_report,is_active,family_member_id_fk,primary_relationship,secondary_relationship)
-Values ('Walhart','The Conq','2012-04-19',190.5,165.3,'987654321','CONW00020004','4383338256','Valm','VM','W4L1H3','Some Skill...',1,1,"Other","Other");
+INSERT INTO ClubMember (first_name,last_name,dob,height,weight,social_sec_num,med_card_num,phone_number,city,province,postal_code,progress_report,is_active,family_member_id_fk,primary_relationship,secondary_relationship)
+VALUES ('Walhart','The Conq','2012-04-19',190.5,165.3,'987654321','CONW00020004','4383338256','Valm','VM','W4L1H3','Some Skill...',1,1,"Other","Other");
 
-Select *
-From ClubMember;
+SELECT *
+FROM ClubMember;
 
 Update ClubMember
-Set first_name = 'Hugh', last_name = 'Mungus'
-Where cmn = 1;
+SET first_name = 'Hugh', last_name = 'Mungus'
+WHERE cmn = 1;
 
-Delete From ClubMember
-Where cmn = 10;
+Delete FROM ClubMember
+WHERE cmn = 10;
+
+-- Query 10: Get details of club members who are currently active and have been associated with at least three different locations and are members for at most three years.
+-- 				Details include Club membership number, first name and last name.
+-- 				Results should be displayed sorted in ascending order by club membership number.
+
+SELECT
+	CM.cmn AS 'MembershipNumber',
+    CM.first_name AS 'MemberFirstName',
+    CM.last_name AS 'MemberLastName'
+FROM ClubMember CM
+WHERE CM.is_active = 1 AND
+	(
+		SELECT COUNT(1)
+		FROM FamilyMemberLocation FML
+        Where CM.family_member_id_fk = FML.family_member_id_fk
+        GROUP BY FML.family_member_id_fk
+	) >= 3 AND
+    (
+		Select COUNT(1)
+        FROM Payment
+        WHERE cmn_fk = CM.cmn
+        GROUP BY effectiveDate
+        HAVING SUM(amount) >= 100.00
+    ) <= 3;
