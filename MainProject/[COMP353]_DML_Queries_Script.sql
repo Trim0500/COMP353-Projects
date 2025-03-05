3f0834f54b2574c54a9512123006278a01f8f3e8
Tristan Lafleur <70395022+Trim0500@users.noreply.github.com>
2025-03-05 13:23:25 -0500

    Fix[COMP353-Main-DML]: Errors in CRUD queries

--- MainProject/[COMP353]_DML_Queries_Script.sql
+++ MainProject/[COMP353]_DML_Queries_Script.sql
@@ -1,7 +1,9 @@
 -- Query 3: CRUD for FamilyMember (Primary/Secondary)
 
-Insert Into FamilyMember
-Values ('Albein "Alm"','Rudolph II','1992-03-14','123456789','RUDA00020003','Ram','Zofia','4383323524','A1M0RG');
+ALTER TABLE FamilyMember AUTO_INCREMENT = 1;
+
+Insert Into FamilyMember (first_name,last_name,dob,social_sec_num,med_card_num,city,province,phone_number,postal_code)
+Values ('Albein "Alm"','Rudolph II','1992-03-14','123456789','RUDA00020003','Ram','ZO','4383323524','A1M0RG');
 
 Select *
 From FamilyMember;
@@ -21,15 +23,17 @@ From SecondaryFamilyMember;
 
 Update SecondaryFamilyMember
 Set first_name = 'Dick', last_name = 'Tree'
-Where id = 1;
+Where primary_family_member_id_fk = 1;
 
 Delete From SecondaryFamilyMember
-Where id = 10;
+Where primary_family_member_id_fk = 10;
 
 -- Query 4: CRUD for ClubMember
 
-Insert Into ClubMember
-Values ('Walhart','The Conq','2012-04-19',190.5,165.3,'987654321','CONW00020004','4383338256','Valm','Valm','W4L1H3','Some Skill...',1,1,"Other","Other");
+ALTER TABLE ClubMember AUTO_INCREMENT = 1;
+
+Insert Into ClubMember (first_name,last_name,dob,height,weight,social_sec_num,med_card_num,phone_number,city,province,postal_code,progress_report,is_active,family_member_id_fk,primary_relationship,secondary_relationship)
+Values ('Walhart','The Conq','2012-04-19',190.5,165.3,'987654321','CONW00020004','4383338256','Valm','VM','W4L1H3','Some Skill...',1,1,"Other","Other");
 
 Select *
 From SecondaryFamilyMember;
