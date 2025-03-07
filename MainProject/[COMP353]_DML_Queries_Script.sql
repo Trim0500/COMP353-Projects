96613e0acbc4b5a8e30c81e6a957cd8442538c41
Tristan Lafleur <70395022+Trim0500@users.noreply.github.com>
2025-03-07 18:04:35 -0500

    Fix[COMP353-Main-DML]: Adjust queries to fit missing conditions

--- MainProject/[COMP353]_DML_Queries_Script.sql
+++ MainProject/[COMP353]_DML_Queries_Script.sql
@@ -2,8 +2,8 @@
 
 ALTER TABLE FamilyMember AUTO_INCREMENT = 1;
 
-INSERT INTO FamilyMember (first_name,last_name,dob,social_sec_num,med_card_num,city,province,phone_number,postal_code)
-VALUES ('Albein "Alm"','Rudolph II','1992-03-14','123456789','RUDA00020003','Ram','ZO','4383323524','A1M0RG');
+INSERT INTO FamilyMember (first_name,last_name,dob,email,social_sec_num,med_card_num,city,province,phone_number,postal_code)
+VALUES ('Albein "Alm"','Rudolph II','1992-03-14','alm.rudolph@rigelnet.com','123456789','RUDA00020003','Ram','ZO','4383323524','A1M0RG');
 
 SELECT *
 FROM FamilyMember;
@@ -32,8 +32,8 @@ WHERE primary_family_member_id_fk = 10;
 
 ALTER TABLE ClubMember AUTO_INCREMENT = 1;
 
-INSERT INTO ClubMember (first_name,last_name,dob,height,weight,social_sec_num,med_card_num,phone_number,city,province,postal_code,progress_report,is_active,family_member_id_fk,primary_relationship,secondary_relationship)
-VALUES ('Walhart','The Conq','2012-04-19',190.5,165.3,'987654321','CONW00020004','4383338256','Valm','VM','W4L1H3','Some Skill...',1,1,"Other","Other");
+INSERT INTO ClubMember (first_name,last_name,dob,email,height,weight,social_sec_num,med_card_num,phone_number,city,province,postal_code,address,progress_report,is_active,family_member_id_fk,primary_relationship,secondary_relationship)
+VALUES ('Walhart','The Conq','2012-04-19','wconq@valmail.com',190.5,165.3,'987654321','CONW00020004','4383338256','Valm','VM','W4L1H3','42 Saber Av.','Some Skill...',1,1,"Other","Other");
 
 SELECT *
 FROM ClubMember;
@@ -82,22 +82,42 @@ SET @endDate = '2025-03-31';
 
 SELECT
 	L.name AS 'LocationName',
-    COUNT(S.id) AS 'SessionsAtLocation',
+    COUNT(
+		CASE
+			WHEN S.event_type = 'training' THEN 1
+            ELSE 0
+        END
+    ) AS 'TrainingSessions',
     (
 		SELECT COUNT(DISTINCT TM.cmn_fk)
 		FROM Session s
         JOIN TeamSession TS ON s.id = TS.session_id_fk
         JOIN TeamFormation TF ON TS.team_formation_id_fk = TF.id
         JOIN TeamMember TM ON TF.id = TM.team_formation_id_fk
-        WHERE s.location_id_fk = L.id
+        WHERE s.location_id_fk = L.id AND s.event_type = 'training'
         GROUP BY s.id
-	) AS 'PlayersInSession'
+	) AS 'PlayersInTrainingSession',
+    COUNT(
+		CASE
+			WHEN S.event_type = 'game' THEN 1
+            ELSE 0
+        END
+    ) AS 'GameSessions',
+    (
+		SELECT COUNT(DISTINCT TM.cmn_fk)
+		FROM Session s
+        JOIN TeamSession TS ON s.id = TS.session_id_fk
+        JOIN TeamFormation TF ON TS.team_formation_id_fk = TF.id
+        JOIN TeamMember TM ON TF.id = TM.team_formation_id_fk
+        WHERE s.location_id_fk = L.id AND s.event_type = 'game'
+        GROUP BY s.id
+	) AS 'PlayersInGameSession'
 FROM Location L
 JOIN Session S ON L.id = S.location_id_fk
 WHERE date_format(S.event_date_time, '%Y-%M-%d') >= @startDate AND date_format(S.event_date_time, '%Y-%M-%d') <= @endDate
 GROUP BY L.id, L.name
-HAVING COUNT(S.id) >= 2
-ORDER BY SessionsAtLocation DESC;
+HAVING GameSessions >= 2
+ORDER BY GameSessions DESC;
 
 -- Query 12: Get a report on all active club members who have never been assigned to any formation team session.
 -- 				The list should include the club member’s membership number, first name, last name, age, date of joining the club, phone number, email and current location name.
@@ -117,7 +137,7 @@ SELECT
         LIMIT 1
     ) AS 'JoinDate',
     CM.phone_number AS 'MemberPhone',
-    -- email,
+    CM.email as 'MemberEmail',
     (
 		SELECT GROUP_CONCAT(L.name ORDER BY L.name SEPARATOR ', ')
         FROM Location L
