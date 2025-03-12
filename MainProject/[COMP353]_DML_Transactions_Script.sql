72fe50fd409a0eef7edd943f51d1777c8db08043
Tristan Lafleur <70395022+Trim0500@users.noreply.github.com>
2025-03-12 13:39:48 -0400

    Task[COMP353-Main-Transactions]: Insert historical personnel data

--- MainProject/[COMP353]_DML_Transactions_Script.sql
+++ MainProject/[COMP353]_DML_Transactions_Script.sql
@@ -129,27 +129,30 @@ VALUES ('Donkey','Kong','1981-07-09','503124158','680969607','5068994178','Kongo
 INSERT INTO PersonnelLocation
 VALUES (1,1,'2021-01-01',null,'General Manager'),
 		(2,1,'2021-01-01',null,'Deputy Manager'),
-        (3,1,'2021-01-01',null,'Treasurer'),
-        (4,1,'2021-01-01',null,'Secretary'),
-        (5,1,'2021-01-01',null,'Admin')
+		(3,1,'2021-01-01',null,'Treasurer'),
+		(4,1,'2021-01-01',null,'Secretary'),
+		(5,1,'2021-01-01',null,'Admin')
 		(6,2,'2022-01-01',null,'Admin'),
 		(7,2,'2022-01-01',null,'Treasurer'),
 		(8,2,'2022-06-01',null,'Coach'),
-        (9,2,'2022-06-01',null,'Assistant Coach'),
-        (10,3,'2022-01-01',null,'Admin'),
+		(9,2,'2022-06-01',null,'Assistant Coach'),
+		(10,3,'2022-01-01',null,'Admin'),
 		(11,3,'2022-01-01',null,'Treasurer'),
 		(12,3,'2022-09-01',null,'Coach'),
-        (13,3,'2022-09-01',null,'Assistant Coach'),
-        (14,4,'2023-01-01',null,'Admin'),
+		(13,3,'2022-09-01',null,'Assistant Coach'),
+		(14,4,'2023-01-01',null,'Admin'),
 		(15,4,'2023-01-01',null,'Treasurer'),
 		(16,4,'2023-01-01',null,'Coach'),
-        (17,4,'2023-01-01',null,'Assistant Coach'),
-        (18,5,'2024-01-01',null,'Admin'),
+		(17,4,'2023-01-01',null,'Assistant Coach'),
+		(18,5,'2024-01-01',null,'Admin'),
 		(19,5,'2024-01-01',null,'Treasurer'),
 		(20,5,'2024-03-01',null,'Coach'),
-        (21,5,'2024-03-01',null,'Assistant Coach'),
-        (22,1,'2022-06-01',null,'Coach'),
-        (23,1,'2022-06-01',null,'Assistant Coach');
+		(21,5,'2024-03-01',null,'Assistant Coach'),
+		(22,1,'2022-06-01',null,'Coach'),
+		(23,1,'2022-06-01',null,'Assistant Coach')
+		(24,1,'2021-01-01','2023-01-25','Admin'),
+		(25,6,'2025-01-01',null,'Admin'),
+		(26,7,'2025-01-01',null,'Admin');
 
 ALTER TABLE ClubMember AUTO_INCREMENT = 1;
 
