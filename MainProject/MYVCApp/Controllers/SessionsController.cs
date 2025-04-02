e8a49c078ab9ab661ceacf87c51063e723680e7f
ICARUS-2 <104460346+ElliptiKeys@users.noreply.github.com>
2025-04-02 15:49:29 -0400

    SessionController documentation done

--- MainProject/MYVCApp/Controllers/SessionsController.cs
+++ MainProject/MYVCApp/Controllers/SessionsController.cs
@@ -11,23 +11,41 @@ using MYVCApp.Models;
 
 namespace MYVCApp.Controllers
 {
+    /// <summary>
+    /// Handles all interactions with Sessions in the database.
+    /// </summary>
     public class SessionsController : Controller
     {
         private readonly ApplicationDbContext _context;
 
+        /// <summary>
+        /// Instantiates controller with injected DbContext.
+        /// </summary>
+        /// <param name="context">Injected DbContext.</param>
         public SessionsController(ApplicationDbContext context)
         {
             _context = context;
         }
 
+        /// <summary>
+        /// Gets the base list view for Sessions.
+        /// </summary>
+        /// <returns>List view for Sessions.</returns>
         [HttpGet]
+        [Route("Sessions")]
         public async Task<IActionResult> Index()
         {
             var applicationDbContext = _context.Sessions.Include(s => s.LocationIdFkNavigation);
             return View(await applicationDbContext.ToListAsync());
         }
 
+        /// <summary>
+        /// Gets the details view for a given Session.
+        /// </summary>
+        /// <param name="id">The primary key of that session.</param>
+        /// <returns>The details view for the session if it exists, 404 otherwise.</returns>
         [HttpGet]
+        [Route("Sessions/{id}")]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null || _context.Sessions == null)
@@ -46,17 +64,28 @@ namespace MYVCApp.Controllers
             return View(session);
         }
 
+        /// <summary>
+        /// Gets the creation view for a new Session.
+        /// </summary>
+        /// <returns>Creation view for a new Session.</returns>
         [HttpGet]
+        [Route("Sessions/Create")]
         public IActionResult Create()
         {
             ViewData["LocationIdFk"] = new SelectList(_context.Locations, "Id", "Id");
             return View();
         }
 
+        /// <summary>
+        /// Creates a new Session in the database.
+        /// </summary>
+        /// <param name="session">The form data.</param>
+        /// <returns>Redirect to List if successful, back to form otherwise.</returns>
         // POST: Sessions/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [Route("Sessions/Create")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,EventType,EventDateTime,LocationIdFk")] Session session)
         {
@@ -84,7 +113,13 @@ namespace MYVCApp.Controllers
             return View(session);
         }
 
+        /// <summary>
+        /// Gets the edit view for a session in the database.
+        /// </summary>
+        /// <param name="id">The primary key of that session.</param>
+        /// <returns>The edit form for the Session if it exists, 404 otherwise.</returns>
         [HttpGet]
+        [Route("Sessions/Edit/{id}")]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.Sessions == null)
@@ -101,10 +136,17 @@ namespace MYVCApp.Controllers
             return View(session);
         }
 
+        /// <summary>
+        /// Edits a given session in the database.
+        /// </summary>
+        /// <param name="id">The primary key of the session to edit.</param>
+        /// <param name="session">The form data.</param>
+        /// <returns>Redirect to List if successful, back to the form otherwise.</returns>
         // POST: Sessions/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [Route("Sessions/Edit/{id}")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,EventType,EventDateTime,LocationIdFk")] Session session)
         {
@@ -147,7 +189,13 @@ namespace MYVCApp.Controllers
             return View(session);
         }
 
+        /// <summary>
+        /// Gets the deletion confirmation view for a given Session.
+        /// </summary>
+        /// <param name="id">The primary key for that session.</param>
+        /// <returns>The deletion view if it exists, 404 otherwise.</returns>
         [HttpGet]
+        [Route("Sessions/Delete/{id}")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null || _context.Sessions == null)
@@ -167,6 +215,11 @@ namespace MYVCApp.Controllers
             return View(session);
         }
 
+        /// <summary>
+        /// Deletes a given session in the database.
+        /// </summary>
+        /// <param name="id">The primary key of that session.</param>
+        /// <returns>Redirect to List if successful, Problem if one occurs.</returns>
         // POST: Sessions/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
