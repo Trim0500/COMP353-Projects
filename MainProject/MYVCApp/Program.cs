6916fd4261874098da85c1d11324382c233a6970
ICARUS-2 <104460346+ElliptiKeys@users.noreply.github.com>
2025-04-02 16:53:54 -0400

    Changed API name

--- MainProject/MYVCApp/Program.cs
+++ MainProject/MYVCApp/Program.cs
@@ -62,7 +62,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "MYVC API V1");
         c.RoutePrefix = "docs";
     });
 }
